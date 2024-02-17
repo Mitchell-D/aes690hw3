19b7c727037c437013aeb0b46fafbe6642623138
Mitchell-D <mtd0012@uah.edu>
2024-02-16 19:17:55 -0600

    Updated workflow ; incorporated variational inferenec ; trained some models

--- model_methods.py
+++ model_methods.py
@@ -1,24 +1,12 @@
+""" Methods for building and training feedforward neural networks """
+import numpy as np
+import json
 from pathlib import Path
 from random import random
-import pickle as pkl
 
-import numpy as np
-import os
-import sys
-import h5py
-import json
-import random as rand
-from list_feats import dynamic_coeffs,static_coeffs
-
-#import keras_tuner
 import tensorflow as tf
-from tensorflow.keras.layers import Layer,Masking,Reshape,ReLU,Conv1D,Add
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dropout, Concatenate, BatchNormalization
-from tensorflow.keras.layers import TimeDistributed, Flatten, RepeatVector
-from tensorflow.keras.layers import InputLayer, LSTM, Dense, Bidirectional
-from tensorflow.keras.regularizers import L2
-from tensorflow.keras import Input, Model
+from tensorflow.keras import Input,Model
+from tensorflow.keras.layers import Dropout, BatchNormalization, Layer, Dense
 
 def_dense_kwargs = {
         "activation":"sigmoid",
@@ -34,7 +22,10 @@ def_dense_kwargs = {
 
 def load_config(model_dir):
     """
-    Load the configuration JSON associated contained in a model directory
+    Load the configuration JSON associated with a specific model.
+
+    :@param model_dir: The directory associated with a specific model instance
+        which contains a {model_name}_config.json describing its hyperparams.
     """
     model_name = model_dir.name
     return json.load(model_dir.joinpath(f"{model_name}_config.json").open("r"))
@@ -42,6 +33,9 @@ def load_config(model_dir):
 def load_csv_prog(model_dir):
     """
     Load the per-epoch metrics from a tensorflow CSVLogger file as a dict.
+
+    :@param model_dir: The directory associated with a specific model instance
+        which contains a {model_name}_prog.csv describing its hyperparams.
     """
     cfg = load_config(model_dir)
     csv_path = model_dir.joinpath(f"{cfg['model_name']}_prog.csv")
@@ -73,107 +67,229 @@ def get_dense_stack(name:str, layer_input:Layer, node_list:list,
         l_prev = l_new
     return l_prev
 
-def basic_dense(name:str, node_list:list, num_window_feats:int,
-        num_horizon_feats:int, num_static_feats:int, num_pred_feats:int,
+def kl_divergence(z_mean, z_log_var):
+    """ """
+    return tf.reduce_mean(
+            z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1
+            ) * -0.5
+
+def get_vi_projection(name:str, layer_input, latent_size:int):
+    """
+    """
+    ## Project to requested latent dimension for predicted mean and variance
+    z_mean = Dense(latent_size, name=f"{name}_mean",
+                   activation="linear")(layer_input)
+    z_log_var = Dense(latent_size, name=f"{name}_logvar",
+                      activation="linear")(layer_input)
+    ## Draw a sample from the distribution from predicted parameters
+    ## shaped like (B,L) for B batch elements and L latent features.
+    epsilon = tf.random.normal(shape=tf.shape(z_mean))
+    sample = z_mean + tf.exp(0.5 * z_log_var) * epsilon
+    ## Concatenate the sample values to a (B,V,L) shape for B elements in the
+    ## batch, V variational features (sample,z_mean,z_log_var), and L latent.
+    return (sample,z_mean,z_log_var)
+
+'''
+class KL_Divergence(Layer):
+    def call(self,inputs):
+        z_sample,z_mean,z_log_var = inputs
+        ## Calculate the KL divergence
+        kl_loss = -0.5 * tf.reduce_mean(
+                z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1
+                )
+        self.add_loss(kl_loss)
+'''
+
+def variational_encoder_decoder(
+        name:str, num_inputs:int, num_outputs:int, latent_size:int,
+        enc_node_list, dec_node_list, dropout_rate=0.0, batchnorm=True,
+        enc_dense_kwargs={}, dec_dense_kwargs={}):
+    """
+    Construct a variational encoder decoder model parameterized by latent_size
+    gaussian distributions approximating the latent posterior p(z|x) of input x
+
+    :@param name: Model name for layer labeling
+    :@param num_inputs: Number of inputs to the model
+    :@param num_outputs: Number of values predicted by the model
+    :@param latent_size: Number of latent distributions.
+    :@param enc_node_list: List of int node counts per layer in the encoder
+    :@param dec_node_list: List of int node counts per layer in the decoder
+    :@param dropout_rate: Dropout rate in [0,1]
+    :@param batchnorm: if True, do batchnorm regularization
+    :@param enc_dense_kwargs: arguments (like activation function) passed to
+        all of the encoder feedforward layers.
+    :@param dec_dense_kwargs: arguments (like activation function) passed to
+        all of the decoder feedforward layers.
+    """
+    l_input = Input(shape=(num_inputs,), name=f"{name}_input")
+    l_enc_dense = get_dense_stack(
+            name=f"{name}_enc",
+            node_list=enc_node_list,
+            layer_input=l_input,
+            dropout_rate=dropout_rate,
+            batchnorm=batchnorm,
+            dense_kwargs=enc_dense_kwargs,
+            )
+    sample,z_mean,z_log_var = get_vi_projection(
+            name=name,
+            layer_input=l_enc_dense,
+            latent_size=latent_size,
+            )
+    l_decoder = get_dense_stack(
+            name=f"{name}_dec",
+            node_list=dec_node_list,
+            layer_input=sample,
+            dropout_rate=dropout_rate,
+            batchnorm=batchnorm,
+            dense_kwargs=dec_dense_kwargs,
+            )
+    l_output = Dense(
+            num_outputs,
+            name=f"{name}_out",
+            activation="linear"
+            )(l_decoder)
+    ved = Model(l_input, l_output)
+    ved.add_loss(kl_divergence(z_mean, z_log_var))
+    return ved
+
+def feedforward(name:str, node_list:list, num_inputs:int, num_outputs:int,
         batchnorm=True, dropout_rate=0.0, dense_kwargs={}):
-    """
-    Dense layer next-step predictor model that simply appends window and
-    horizon feats as the input, and outputs the prediction features for the
-    next time step. The only reason there's a distinction between window and
-    horizon features is to conform to the input tensor style used by others.
-    """
-    w_in = Input(shape=(1,num_window_feats,), name="in_window")
-    h_in = Input(shape=(1,num_horizon_feats,), name="in_horizon")
-    s_in = Input(shape=(num_static_feats,), name="in_static")
-    mod_in = Reshape(target_shape=(1,num_static_feats))(s_in)
-    all_in = Concatenate(axis=-1)([w_in,h_in,mod_in])
+    """ Just a series of dense layers with some optional parameters """
+    ff_in = Input(shape=(num_inputs,), name="input")
     dense = get_dense_stack(
             name=name,
             node_list=node_list,
-            layer_input=all_in,
+            layer_input=ff_in,
             dropout_rate=dropout_rate,
             batchnorm=batchnorm,
             dense_kwargs=dense_kwargs,
             )
-    output = Dense(units=num_pred_feats,
-            activation="linear",name="output")(dense)
-    inputs = {"window":w_in,"horizon":h_in,"static":s_in}
-    model = Model(inputs=inputs, outputs=[output])
+    output = Dense(units=num_outputs, activation="linear",name="output")(dense)
+    model = Model(inputs=ff_in, outputs=output)
     return model
 
-def get_sample_generator(train_h5s,val_h5s,window_size,horizon_size,
-        window_feats,horizon_feats,pred_feats,static_feats):
+def gen_noisy(X, Y, noise_pct=0, noise_stdev=0, mask_val=9999.,
+              feat_probs:np.array=None, shuffle=True, rand_seed=None):
     """
-    Returns generators which provide window, horizon, and static data
-    as features, and prediction data as labels by subsetting a larger
-    sequence per-sample.
-    """
-    ## Nested output signature for gen_hdf5_sample
-    out_sig = ({
-        "window":tf.TensorSpec(
-            shape=(window_size,len(window_feats)), dtype=tf.float64),
-        "horizon":tf.TensorSpec(
-            shape=(horizon_size,len(horizon_feats)), dtype=tf.float64),
-        "static":tf.TensorSpec(
-            shape=(len(static_feats),), dtype=tf.float64)
-        },
-        tf.TensorSpec(shape=(horizon_size,len(pred_feats)), dtype=tf.float64))
+    Generates (X, Y, sample_weight) triplets for training a model with maskable
+    feature values. The percentage of masked values determines the weight.
 
-    pos_args = (
-            window_size,horizon_size,
-            window_feats,horizon_feats,
-            pred_feats,static_feats
+    The feature dimension is always assumed to be the last one. For example...
+
+    (S,F) is a dataset with F features measured in S samples
+    (S,L,F) is a dataset with S sequences of L members, each having F features.
+    (S,M,N,F) is a 2d (M,N) dataset with F features and S sample instances.
+
+    :@param X: (S,F) array of S samples of F features.
+    :@param Y: (S,P) array of S samples of P predicted values.
+    :@param noise_pct: mean percentage of feature values to mask.
+    :@param noise_stdev: standard deviation of feature values to mask.
+    :@param mask_val: Value to substitute for the mask.
+    :@param feat_probs: Array with the same size as number of features, which
+        provides the relative probabilities of each feature being selected to
+        be masked. Uniform by default.
+    :@param shuffle: if True, randomly shuffles samples along the first axis.
+    """
+    num_feats = X.shape[-1]
+    num_samples = X.shape[0]
+    ## Make sure the feature probability array is formatted correctly
+    if feat_probs is None:
+        feat_probs = np.full(shape=(num_feats,), fill_value=1.)
+    else:
+        assert np.array(feat_probs).squeeze().shape == (num_feats,)
+        assert np.all(feat_probs >= 0.)
+    ## Shuffle along the sample axis, if requested.
+    if shuffle:
+        rand_idxs = np.arange(num_samples)
+        np.random.seed(rand_seed)
+        np.random.shuffle(rand_idxs)
+        X = X[rand_idxs]
+        Y = Y[rand_idxs]
+    ## Preserve ratios and convert to probabilities summing to 1
+    feat_probs = feat_probs / np.sum(feat_probs)
+    ## Pick a number of features to mask according to a distribution of
+    ## percentages saturating at 0 and 1, parameterized by the user.
+    noise_dist = np.random.normal(noise_pct,noise_stdev,size=num_samples)
+    mask_count = np.rint(np.clip(noise_dist,0,1)*num_feats).astype(int)
+    feat_idxs = np.arange(num_feats).astype(int)
+    ##(!!!) The feature dimension is always assumed to be the final one (!!!)##
+    for i in range(num_samples):
+        ## Choose indeces that will be masked in each sample
+        mask_idxs = np.random.choice(
+                feat_idxs,
+                size=mask_count[i],
+                replace=False,
+                #p=feat_probs,
+                )
+        X[i,...,mask_idxs] = mask_val
+    weights = 1-mask_count/num_feats
+    for i in range(num_samples):
+        yield (X[i], Y[i], weights[i])
+
+def get_generators(X, Y, tv_ratio=.8, noise_pct=0, noise_stdev=0,
+                   mask_val=9999., feat_probs:np.array=None, shuffle=True,
+                   rand_seed=None, dtype=tf.float64):
+    """
+    Get training and validation dataset generators for 3-tuples like (x,y,w)
+    for input x, true output y, and sample weight w. Optionally use a random
+    masking strategy to corrupt a subset of the features, adjusting the sample
+    weight proportional to the percentage of values that were masked.
+
+    :@param X: Inputs as an array with shape like (S, ... ,F)
+        for S samples and F input features. May be 2+ dimensional.
+    :@param Y: Truth outputs as an array with shape like (S, ... ,P)
+        for S samples and P predicted features. May be 2+ dimensional.
+    :@param tv_ratio: Ratio of training samples to total (sans validation)
+    :@param noise_pct: mean percentage of feature values to mask.
+    :@param noise_stdev: standard deviation of feature values to mask.
+    :@param mask_val: Value to substitute for the mask.
+    :@param feat_probs: Array with the same size as number of features, which
+        provides the relative probabilities of each feature being selected to
+        be masked. Uniform by default.
+    :@param shuffle: if True, randomly shuffles samples along the first axis.
+    """
+    num_samples = X.shape[0]
+    num_feats = X.shape[-1]
+    assert Y.shape[0] == num_samples
+    if shuffle:
+        rand_idxs = np.arange(num_samples)
+        np.random.seed(rand_seed)
+        np.random.shuffle(rand_idxs)
+        X = X[rand_idxs]
+        Y = Y[rand_idxs]
+        split_idx = np.array([int(tv_ratio*num_samples)])
+    Tx,Vx = np.split(X, split_idx)
+    Ty,Vy = np.split(Y, split_idx)
+    out_sig = tf.TensorSpec(shape=Y.shape, dtype=dtype)
+    #'''
+    out_sig = (
+            tf.TensorSpec(shape=Tx.shape[1:],dtype=dtype),
+            tf.TensorSpec(shape=Ty.shape[1:], dtype=dtype),
+            tf.TensorSpec(shape=tuple(), dtype=dtype),
             )
+    #'''
+    if feat_probs is None:
+        feat_probs = np.full(shape=(num_feats,), fill_value=1.)
     gen_train = tf.data.Dataset.from_generator(
-            gen_sample,
-            args=(train_h5s, *pos_args),
+            gen_noisy,
+            args=(Tx,Ty,noise_pct,noise_stdev,mask_val, feat_probs, shuffle),
             output_signature=out_sig,
             )
     gen_val = tf.data.Dataset.from_generator(
-            gen_sample,
-            args=(val_h5s, *pos_args),
+            gen_noisy,
+            args=(Vx,Vy,noise_pct,noise_stdev,mask_val, feat_probs, shuffle),
             output_signature=out_sig,
             )
     return gen_train,gen_val
 
 if __name__=="__main__":
-    sample_dir = Path("/rstor/mdodson/thesis")
-    h5s_val = [sample_dir.joinpath("shuffle_2018.h5").as_posix()]
-    h5s_train = [sample_dir.joinpath(f"shuffle_{y}.h5").as_posix()
-        for y in [2015,2019,2021]]
-    window_feats = [
-            "lai", "veg", "tmp", "spfh", "pres", "ugrd", "vgrd",
-            "dlwrf", "ncrain", "cape", "pevap", "apcp", "dswrf",
-            "soilm-10", "soilm-40", "soilm-100", "soilm-200"]
-    horizon_feats = [
-            "lai", "veg", "tmp", "spfh", "pres", "ugrd", "vgrd",
-            "dlwrf", "ncrain", "cape", "pevap", "apcp", "dswrf"]
-    pred_feats = ['soilm-10', 'soilm-40', 'soilm-100', 'soilm-200']
-    static_feats = ["pct_sand", "pct_silt", "pct_clay", "elev", "elev_std"]
-
-    g = gen_sample(
-            h5_paths=h5s_train,
-            window_size=24,
-            horizon_size=24,
-            window_feats=window_feats,
-            horizon_feats=horizon_feats,
-            pred_feats=pred_feats,
-            static_feats=static_feats,
-            as_tensor=False,
+    ved = get_ved(
+            num_inputs=10,
+            num_outputs=2,
+            latent_size=8,
+            enc_node_list=[16,32,64,64,32],
+            dec_node_list=[32,64,64,32,16],
+            dropout_rate=0.0,
+            batchnorm=True,
             )
-
-    #for i in range(10000):
-    #    x,y = next(g)
-    #    print([x[k].shape for k in x.keys()], y.shape)
-    gT,gV = get_sample_generator(
-            train_h5s=h5s_train,
-            val_h5s=h5s_val,
-            window_size=24,
-            horizon_size=24,
-            window_feats=window_feats,
-            horizon_feats=horizon_feats,
-            pred_feats=pred_feats,
-            static_feats=static_feats,
-            )
-    batches = [next(g) for i in range(2048)]
-    pkl.dump(batches, Path("data/sample/batch_samples.pkl").open("wb"))
+    ved.summary()
--- train_variational.py
+++ train_variational.py
@@ -31,20 +31,24 @@ if len(gpus):
     tf.config.experimental.set_memory_growth(gpus[0], True)
 #'''
 
-def train_feedforward(X, Y, model_config, model_dir):
+def train_variational(X, Y, model_config, model_dir):
     """
-    Train a feedforward model according to a config dictionary keys below.
+    Train a variational model according to a config dictionary with the keys:
 
     "model_name": unique
     "input_feats": subset of ASOS features to use as network inputs
     "output_feats": subset of ASOS features to predict with the network
+    "latent_size": Number of posterior-approximating gaussian distriubtions
     "source_csv": string path to a ASOS monthly csv file
     "batch_size": int minibatch size (samples per weight update)
     "batch_buffer": int num of batches to preload in memory from the generator
     "dropout_rate": float rate at which neurons are disabled during training
     "batchnorm": bool whether to batch-normalize after layers
-    "dense_kwargs":Keyword arguments for keras Dense layers
-    "node_list":List of ints representing the number of outputs per layer
+    "weighted_metrics": List of metric labels to  be weighted by masking level
+    "enc_dense_kwargs":Keyword arguments for encoder Dense layers
+    "dec_dense_kwargs":Keyword arguments for decoder Dense layers
+    "enc_node_list":List of ints representing the layers in the encoder
+    "dec_node_list":List of ints representing the layers in the decoder
     "loss":String representing the loss function to use per keras labels
     "metrics":List of strings representing metrics to record per keras labels
     "max_epochs": int maximum number of epochs to train
@@ -74,19 +78,24 @@ def train_feedforward(X, Y, model_config, model_dir):
     :@param model_config: Configuration dictionary defining the above terms.
     :@param model_dir: Parent directory where this model's dir will be created
     """
+    assert X.shape[-1] == len(config["input_feats"])
+    assert Y.shape[-1] == len(config["output_feats"])
     ## Initialize the model according to the configuration
-    model = mm.feedforward(
-            name=config.get("model_name"), ## feedforward
-            node_list=config.get("node_list"),
-            num_inputs=len(config.get("input_feats")),
-            num_outputs=len(config.get("output_feats")),
-            batchnorm=config.get("batchnorm"),
-            dropout_rate=config.get("dropout_rate"),
-            dense_kwargs=config.get("dense_kwargs"),
+    model = mm.variational_encoder_decoder(
+            name=config["model_name"],
+            num_inputs=len(config["input_feats"]),
+            num_outputs=len(config["output_feats"]),
+            latent_size=config["latent_size"],
+            enc_node_list=config["enc_node_list"],
+            dec_node_list=config["dec_node_list"],
+            batchnorm=config["batchnorm"],
+            dropout_rate=config["dropout_rate"],
+            enc_dense_kwargs=config["enc_dense_kwargs"],
+            dec_dense_kwargs=config["dec_dense_kwargs"],
             )
 
     ## Create and add some information to the dedicated model directory
-    model_dir = model_parent_dir.joinpath(config.get("model_name"))
+    model_dir = model_parent_dir.joinpath(config["model_name"])
     assert not model_dir.exists()
     model_dir.mkdir()
     model_json_path = model_dir.joinpath(f"{config['model_name']}_config.json")
@@ -94,14 +103,14 @@ def train_feedforward(X, Y, model_config, model_dir):
 
     ## Write a model summary to stdout and to a file
     model.summary()
-    summary_path = model_dir.joinpath(config.get("model_name")+"_summary.txt")
+    summary_path = model_dir.joinpath(config["model_name"]+"_summary.txt")
     with summary_path.open("w") as f:
         model.summary(print_fn=lambda x: f.write(x + '\n'))
 
     ## Compile the model
     model.compile(
             optimizer=tf.keras.optimizers.Adam(
-                learning_rate=config.get("learning_rate")),
+                learning_rate=config["learning_rate"]),
             loss=config.get("loss"),
             metrics=config.get("metrics"),
             weighted_metrics=config.get("weighted_metrics"),
@@ -111,11 +120,11 @@ def train_feedforward(X, Y, model_config, model_dir):
     gT,gV = mm.get_generators(
             X=X,
             Y=Y,
-            tv_ratio=config.get("train_val_ratio"),
-            noise_pct=config.get("mask_pct"),
-            noise_stdev=config.get("mask_pct_stdev"),
-            mask_val=config.get("mask_val"),
-            feat_probs=config.get("mask_feat_probs"),
+            tv_ratio=config["train_val_ratio"],
+            noise_pct=config["mask_pct"],
+            noise_stdev=config["mask_pct_stdev"],
+            mask_val=config["mask_val"],
+            feat_probs=config["mask_feat_probs"],
             shuffle=True,
             dtype=tf.float64
             )
@@ -123,14 +132,14 @@ def train_feedforward(X, Y, model_config, model_dir):
     ## Define callbacks for model progress tracking
     callbacks = [
             tf.keras.callbacks.EarlyStopping(
-                monitor=config.get("early_stop_metric"),
-                patience=config.get("early_stop_patience")
+                monitor=config["early_stop_metric"],
+                patience=config["early_stop_patience"]
                 ),
             tf.keras.callbacks.ModelCheckpoint(
                 monitor="val_loss",
                 save_best_only=True,
                 filepath=model_dir.joinpath(
-                    config['model_name']+"_{epoch}_{val_mae:.02f}.hdf5")
+                    config['model_name']+"_{epoch:03}_{val_mae:.03f}.hdf5")
                 ),
             tf.keras.callbacks.CSVLogger(
                 model_dir.joinpath(f"{config['model_name']}_prog.csv"),
@@ -138,16 +147,16 @@ def train_feedforward(X, Y, model_config, model_dir):
             ]
     ## Train the model on the generated tensors
     hist = model.fit(
-            gT.batch(config.get("batch_size")).prefetch(config.get("batch_buffer")),
-            epochs=config.get("max_epochs"),
+            gT.batch(config["batch_size"]).prefetch(config["batch_buffer"]),
+            epochs=config["max_epochs"],
             ## Number of batches to draw per epoch. Use full dataset by default
-            #steps_per_epoch=config.get("train_steps_per_epoch"),
-            validation_data=gV.batch(config.get("batch_size")
-                                     ).prefetch(config.get("batch_buffer")),
+            #steps_per_epoch=config["train_steps_per_epoch"],
+            validation_data=gV.batch(config["batch_size"]
+                                     ).prefetch(config["batch_buffer"]),
             ## batches of validation data to draw per epoch
-            #validation_steps=config.get("val_steps_per_epoch"),
+            #validation_steps=config["val_steps_per_epoch"],
             ## Number of epochs to wait between validation runs.
-            validation_freq=config.get("val_frequency"),
+            validation_freq=config["val_frequency"],
             callbacks=callbacks,
             verbose=2,
             )
@@ -155,15 +164,14 @@ def train_feedforward(X, Y, model_config, model_dir):
     ## Save the most performant models.
     ## (!!!) This relies on the checkpoint file name formatting string, (!!!)
     ## (!!!) and on that there are no other hdf5 files in the model dir (!!!)
-    model_dir = model_parent_dir.joinpath(config.get("model_name"))
     best_model = list(sorted(
         [q for q in model_dir.iterdir() if q.suffix==".hdf5"],
         key=lambda p:int(p.stem.split("_")[1])
         )).pop(-1)
     ## Save the model after further training without improvement
-    model.save(model_dir.joinpath(config.get("model_name")+"_keras.hdf5"))
+    model.save(model_dir.joinpath(config["model_name"]+"_keras.hdf5"))
     ## Save the previous best-performing model from the last checkpoint
-    best_model.rename(model_dir.joinpath(config.get("model_name")+"_final.hdf5"))
+    best_model.rename(model_dir.joinpath(config["model_name"]+"_final.hdf5"))
 
 if __name__=="__main__":
     """ Directory with sub-directories for each model. """
@@ -171,47 +179,52 @@ if __name__=="__main__":
     model_parent_dir = data_dir.joinpath("models")
     asos_al_path = data_dir.joinpath("AL_ASOS_July_2023.csv")
     asos_ut_path = data_dir.joinpath("UT_ASOS_Mar_2023.csv")
-
     config = {
-            "model_name":"ff-5",
-            "input_feats":["tmpc","relh","mslp"],
+            "model_name":"ved-3",
+            "input_feats":["tmpc","dwpc","relh","sknt",
+                "mslp","p01m","gust","feel"],
             "output_feats":["romps_LCL_m","lcl_estimate"],
             "source_csv":asos_al_path.as_posix(),
-            "batch_size":64,
+            "batch_size":32,
             "batch_buffer":4,
-            "dropout_rate":0.1,
+            "dropout_rate":0.0,
             "batchnorm":True,
             "dense_kwargs":{"activation":"sigmoid"},
-            "node_list":[64,32,32,32,16,16],
+            "enc_node_list":[128,128,128,64,32],
+            "latent_size":8,
+            "dec_node_list":[32,64,64,16],
             "loss":"mse",
             "metrics":["mse", "mae"],
+            "enc_dense_kwargs":{"activation":"relu"},
+            "dec_dense_kwargs":{"activation":"relu"},
+            "weighted_metrics":["mae", "mse"],
             "max_epochs":128, ## maximum number of epochs to train
             "train_steps_per_epoch":16, ## number of batches per epoch
-            "weighted_metrics":["mae", "mse"], ## Also recorded in prog csv
             "val_steps_per_epoch":3, ## number of batches per validation
             "val_frequency":1, ## epochs between validation
-            "learning_rate":1e-5,
+            "learning_rate":1e-2,
             "early_stop_metric":"val_mse", ## metric evaluated for stagnation
             "early_stop_patience":30, ## number of epochs before stopping
-            "mask_pct":.1,
-            "mask_pct_stdev":.2,
+            "mask_pct":0,
+            "mask_pct_stdev":0,
             "mask_val":9999.,
             "train_val_ratio":.8,
             "mask_feat_probs":None,
-            "notes":"Basic model ; Some masking ; Only key parameters ; some dropout",
+            "notes":"Same architecture; NO MASKING; very fast learning rate",
             }
 
     from preprocess import preprocess
     data_dict = preprocess(
             asos_csv_path=asos_al_path,
-            input_feats=config.get("input_feats"),
-            output_feats=config.get("output_feats"),
+            input_feats=config["input_feats"],
+            output_feats=config["output_feats"],
             normalize=True,
             )
 
-    train_feedforward(
+    train_variational(
             X=data_dict["X"],
             Y=data_dict["Y"],
             model_config=config,
             model_dir=model_parent_dir,
             )
+
